3ba8df4f7f525a15f49cf946e883f81be4945907
Prajith Parammal <prajithparammal@gmail.com>
2019-10-22 22:49:07 +0800

    first commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+
+variable "client_id" {
+  description = "The Client ID for the Service Principal. Changing this forces a new resource to be created."
+}
+
+variable "client_secret" {
+  description = "The Client Secret for the Service Principal. Changing this forces a new resource to be created."
+}
+
+variable "subscription_id" {
+  description = "Enter Subscription ID for provisioning resources in Azure"
+}
+
+variable "tenant_id" {
+  description = "Enter tenant ID/Directory ID of your Azure AD"
+}
